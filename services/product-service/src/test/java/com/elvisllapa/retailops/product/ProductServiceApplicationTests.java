f83b5f3ecd50d7de5e22120d4e1268e186d74d6d
Elvis Llapa <elvisllapa34@gmail.com>
2026-07-13 20:23:51 -0500

    feat: initialize product service

---
+++ services/product-service/src/test/java/com/elvisllapa/retailops/product/ProductServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.elvisllapa.retailops.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=" +
+    "org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration," +
+    "org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration"
+})
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
